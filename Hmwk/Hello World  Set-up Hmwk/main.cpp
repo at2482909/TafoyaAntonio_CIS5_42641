a6442d15ab9e391abfc0f05f8b74dd838ce58b76
at2482909 <atafoya5@student.rcc.edu>
2017-02-24 21:22:27 -0800

    Signed-off-by: at2482909 <atafoya5@student.rcc.edu>

---
+++ Hmwk/Hello World  Set-up Hmwk/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Antonio Tafoya
+ * Purpose: Create my first program. Trying to feel confident in understanding 
+ * this.I feel as if I was tossed into another planet lol.
+ *
+ * Created on February 24, 2017, 9:09 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    cout << "Hello World\n";
+    return 0;
+}
+
---
+++ Hmwk/Hello World  Set-up Hmwk/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=hello_world__set-up_hmwk
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/hello_world__set-up_hmwk
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=helloworldset-uphmwk.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/helloworldset-uphmwk.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=hello_world__set-up_hmwk
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/hello_world__set-up_hmwk
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=helloworldset-uphmwk.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/helloworldset-uphmwk.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
