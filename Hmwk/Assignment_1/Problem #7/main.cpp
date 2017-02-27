25940217145c9724fcb24901cae3ba8d5d1129c0
at2482909 <atafoya5@student.rcc.edu>
2017-02-26 19:44:52 -0800

    Signed-off-by: at2482909 <atafoya5@student.rcc.edu>

---
+++ Hmwk/Assignment_1/Problem #7/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Anthony
+ *
+ * Created on February 26, 2017, 7:18 PM
+ */
+
+#include <iostream>
+ using namespace std;
+ int main( )
+ {
+    
+   cout << "*****************************************************\n";         
+cout << "               C   C   C         S  S  S  S            !!\n";
+cout << "              C          C      S          S           !!\n"; 
+cout<< "              C                 S                       !!\n";
+cout<< "              C                 S                       !!\n";
+cout << "             C                  S S S S                !!\n";   
+cout << "              C                           S            !!\n";
+cout << "               C                           S           !!\n";
+cout << "                C          C      S          S\n";
+cout << "                 C   C   C         S  S  S  S          OO\n";
+cout << "*****************************************************\n";
+cout << "Computer Science is Cool Stuff!!!\n";
+return 0;
+ }
+
